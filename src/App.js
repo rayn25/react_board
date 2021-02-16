804be14a814385b694aa07f368047160d31b6a5b
username <user@email.mail>
2021-02-16 13:33:04 +0900

    20210216

--- src/App.js
+++ src/App.js
@@ -1,25 +1,151 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+//import { Route, BrowserRouter as Router } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import App1 from './App1';
+// import App2 from './App2';
+// import App3 from './App3';
+// import App4 from './App4';
+// import App5 from './App5';
+// import App6 from './App6';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.child = React.createRef();
+    }
+
+  state = {
+      maxNo: 3,
+      boards:  [
+          {
+              brdno: 1,
+              brdwriter: 'Lee SunSin',
+              brdtitle: 'If you intend to live then you die',
+              brddate: new Date()
+          },
+          {
+              brdno: 2,
+              brdwriter: 'So SiNo',
+              brdtitle: 'Founder for two countries',
+              brddate: new Date()
+          }
+      ]
+  }
+
+  handleSaveData = (data) => {
+      let boards = this.state.boards;
+      if(data.brdno === null || data.brdno === '' || data.brdno === undefined) // new : insert
+          this.setState({
+             maxNo: this.state.maxNo+1,
+             boards: boards.concat({ brdno: this.state.maxNo, brddate: new Date(), ...data})
+          });
+      else {
+          this.setState({
+              boards: boards.map(row => data.brdno === row.brdno ? {...data}: row)
+          })
+      }
+  }
+
+  handleRemove = (brdno) => {
+      this.setState({
+          boards: this.state.boards.filter(row => row.brdno !== brdno)
+      })
+  }
+
+  handleSelectRow = (row) => {
+    this.child.current.handleSelectRow(row);
+  }
+
+  render() {
+      const { boards } = this.state;
+      //const boards = this.state.boards;
+      // const list = boards.map(function(row){
+      //     return row.brdno + row.brdwriter;
+      // });
+
+      return (
+          <div>
+              <BoardForm onSaveData={this.handleSaveData} ref={this.child}/>
+              <table border="1">
+                  <tbody>
+                  <tr align="center">
+                      <td width="50">No.</td>
+                      <td width="300">Title</td>
+                      <td width="100">Name</td>
+                      <td width="100">Date</td>
+                  </tr>
+                  {
+                      boards.map(row =>
+                          (<BoardItem key={row.brdno} row={row} onRemove={this.handleRemove} onSelectRow={this.handleSelectRow}/>)
+                      )
+                  }
+                  </tbody>
+              </table>
+          </div>
+      )
+  }
+}
+
+class BoardItem extends React.Component {
+    handleRemove = () => {
+        const { row, onRemove } = this.props;
+        onRemove(row.brdno);
+    }
+
+    handleSelectRow = () => {
+        const { row, onSelectRow } = this.props;
+        onSelectRow(row);
+    }
+
+    render() {
+        console.log(this.props.row.brdno);
+        return (
+            <tr>
+                <td>{this.props.row.brdno}</td>
+                <td><a onClick={this.handleSelectRow}>{this.props.row.brdtitle}</a></td>
+                <td>{this.props.row.brdwriter}</td>
+                <td>{this.props.row.brddate.toLocaleDateString('ko-KR')}</td>
+                <td><button onClick={this.handleRemove}>X</button></td>
+            </tr>
+        )
+    }
+}
+
+class BoardForm extends Component {
+    state = {
+        brdwriter:'',
+        brdtitle:''
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.onSaveData(this.state);
+        this.setState({
+            brdno:'',
+            brdwriter:'',
+            brdtitle:''
+        });
+    }
+
+    handleSelectRow = (row) => {
+        this.setState(row);
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input placeholder="title" name="brdtitle" value={this.state.brdtitle} onChange={this.handleChange}/>
+                <input placeholder="name" name="brdwriter" value={this.state.brdwriter} onChange={this.handleChange}/>
+                <button type="submit">Save</button>
+            </form>
+        );
+    }
 }
 
 export default App;
